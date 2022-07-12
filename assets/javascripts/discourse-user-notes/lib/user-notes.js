d4fe487e8d53da03c2128843626620406401e1ec
Jarek Radosz <jradosz@gmail.com>
2022-07-13 01:30:10 +0200

    DEV: Loading sanitizer was deprecated 6 years ago (#61)
    in https://github.com/discourse/discourse/commit/3d2180502222377b69be88dcc1e9e90b1e0bf1ae

--- assets/javascripts/discourse-user-notes/lib/user-notes.js
+++ assets/javascripts/discourse-user-notes/lib/user-notes.js
@@ -1,25 +1,22 @@
 import showModal from "discourse/lib/show-modal";
-import loadScript from "discourse/lib/load-script";
 
 export function showUserNotes(store, userId, callback, opts) {
   opts = opts || {};
 
-  return loadScript("defer/html-sanitizer-bundle").then(() => {
-    return store.find("user-note", { user_id: userId }).then((model) => {
-      const controller = showModal("user-notes", {
-        model,
-        title: "user_notes.title",
-        addModalBodyView: true,
-      });
-      controller.reset();
-
-      controller.setProperties({
-        userId,
-        callback,
-        postId: opts.postId,
-      });
-
-      return controller;
+  return store.find("user-note", { user_id: userId }).then((model) => {
+    const controller = showModal("user-notes", {
+      model,
+      title: "user_notes.title",
+      addModalBodyView: true,
     });
+    controller.reset();
+
+    controller.setProperties({
+      userId,
+      callback,
+      postId: opts.postId,
+    });
+
+    return controller;
   });
 }
